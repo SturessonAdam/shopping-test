15fac37c30ff6bf2316c3ca5d46861d2cd3f0683
Adam <Adam.sturesson@live.se>
2024-10-29 09:45:33 +0100

    button testing

--- src/components/Counter.cy.jsx
+++ src/components/Counter.cy.jsx
@@ -10,10 +10,36 @@ describe('<Counter />', () => {
       cy.mount(<Counter />)
     })
 
-    describe('det ska finnas knappar med texten "+" och "-" och "suprise me"', () => {
+    describe('Det ska finnas knappar med texten "+" och "-" och "suprise me"', () => {
 
         it('should have + button', () => {
-            cy.get(".add-button").contains("+");
+            const plus = cy.get(".add-button").contains("+");
+            plus.should("be.visible");
+        })
+        it("should have - button", () => {
+            cy.get(".subtract-button").contains("-").should("be.visible");
+        })
+        it("should have random button", () => {
+            cy.get(".random-button").contains("suprise me").should("be.visible")
+        })
+    })
+
+    describe('Användaren ska se talet 1 initialt (innnan man klickar på knappen)', () => {
+
+        it('should display 1 initially', () => {
+            cy.get('.value').contains(1).should("be.visible");
+        })
+    })
+
+    describe('1. "+" button ', () => {
+        let button;
+        beforeEach(() => {
+            button = cy.get(".add-button");
+        })
+
+        it('should display number 2 when the + buttton is pressed the first time', () => {
+            button.click();
+            cy.get(".value").contains(2).should("be.visible");
         })
     })
 
