4afee0d69e6bad6094ce698e3427ed7f50258ef7
Adam <Adam.sturesson@live.se>
2024-10-29 09:26:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Counter from './components/Counter'
+
+//Kundens krav:
+// -öka och minska antalet produkter av en viss sort i kundvagnen
+
+function App() {
+
+  return (
+    <>
+    <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+
+
+const Counter = () => {
+    const [count, setCount] = useState(1);
+
+    const handleAdd = () => setCount(count +1);
+    const handleSubtract = () => setCount(count -1);
+    const handleRandom = () => {
+        setCount( Math.floor(Math.random() * 101))
+    }
+
+    return (
+        <div className ="counter">
+            Product counter
+            <p className="value">{count}</p>
+            <button className="add button" onClick={handleAdd}> + </button>
+            <button className="subtract button" onClick={handleSubtract}> - </button>
+            <button className="random button" onClick={handleRandom}> suprise me </button>
+        </div>
+    )
+}
+
+export default Counter;
